81efdc4a278e53650545657c018d7fbe1d7c8dda
piero512 <ulloapiero@gmail.com>
2019-02-10 16:03:06 -0500

    Añadir tipo de evento a evento

---
+++ db/migrate/20190210205922_add_event_type_to_event.rb
@@ -0,0 +1,5 @@
+class AddEventTypeToEvent < ActiveRecord::Migration[5.2]
+  def change
+    add_column :events, :tipo, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_192710) do
+ActiveRecord::Schema.define(version: 2019_02_10_205922) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_02_10_192710) do
     t.datetime "due_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tipo"
   end
 
   create_table "members", force: :cascade do |t|
