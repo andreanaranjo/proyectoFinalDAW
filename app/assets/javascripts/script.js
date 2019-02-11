763ef558bf5588dfffb7ab182b488e24662431bc
Vivi Laurido <vivilaurido97@hotmail.com>
2019-02-11 15:00:54 -0500

    Adding old pages

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,5 @@
+function notificar(){
+	var mensaje = "Se han enviado sus datos correctamente";
+	window.confirm(mensaje);
+	location.reload();
+}
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,19 @@ Rails.application.routes.draw do
   root to: "admin/dashboard#index"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  #get 'pages/home'
+
+  root 'pages#home'
+  get 'quienes_somos'  => 'pages#quienes_somos'
+  get 'home' => 'pages#home'
+  get 'que_hacemos' => "pages#que_hacemos"
+  get 'proximos_eventos' => "pages#proximos_eventos"
+  get 'contactenos' => "pages#contactenos"
+  get '/admins/sign_up' => 'devise/registrations#new'
+  get '/admins/sign_in' => 'devise/sessions#create'
+  get '/admins/sign_out' => 'devise/sessions#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
