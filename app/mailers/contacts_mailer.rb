29ddfa22e838bdf602136e1efda0b5287c41619b
Vivi Laurido <vivilaurido97@hotmail.com>
2019-02-12 10:59:53 -0500

    Mail working

--- app/mailers/contacts_mailer.rb
+++
@@ -1,8 +0,0 @@
-class ContactsMailer < ActionMailer::Base
-    default from: "lauridoviviana@gmail.com"
-    
-    def general_message(contact)
-      @contact = contact
-      mail( :to => "lauridoviviana@gmail.com", :subject => "You Have a Message From Your Website")
-    end
-end
--- app/models/contact.rb
+++
@@ -1,9 +0,0 @@
-class Contact < ActiveRecord::Base
-    validates :email, 
-      :presence => :true,
-      :format => { 
-        :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
-        :message => "must be a valid email address"
-      }
-    validates :message, :presence => :true
-end
